06e4f9faeaf2384419224ec7a50cc64489df489d
shikha mahar <shikhamahar27@gmail.com>
2024-08-09 13:50:32 +0530

    Weather Card using React

---
+++ practice_react/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Components/WeatherCard.css'
+import WeatherCard from './Components/WeatherCard'
+
+const App = () => {
+  return (
+    <>
+      <WeatherCard location={"Agra"} />
+    
+    </>
+  )
+}
+
+export default App
